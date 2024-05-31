8d74d16b0e66c44a8732f3d210d67224b2f70665
Demon <3443002624@qq.com>
2024-05-31 17:39:51 +0800

    Update 优化

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,30 @@
 import {
   Controller,
+  Get,
+  Header,
+  Query,
+  Req,
   Post,
   UseInterceptors,
   UploadedFiles,
   Body,
 } from "@nestjs/common";
 import { FilesInterceptor } from "@nestjs/platform-express";
-import { MulterModule } from "@nestjs/platform-express";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get("users")
+  getUsers(@Query("a") a: string): Services.User[] {
+    console.log("%c ~ name ccc ~ ", "color:#2ecc71", a);
+    return this.appService.getUsers(a);
+  }
 
   @Post("users")
   @UseInterceptors(FilesInterceptor("file", 3))

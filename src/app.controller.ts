9ec1d2b97cb896123210462e0ddf0e72f02c7fe9
Demon <3443002624@qq.com>
2024-05-31 17:18:03 +0800

    Update file operation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,31 +1,25 @@
 import {
-  Body,
   Controller,
-  Get,
-  Header,
   Post,
-  Query,
-  Req,
+  UseInterceptors,
+  UploadedFiles,
+  Body,
 } from "@nestjs/common";
+import { FilesInterceptor } from "@nestjs/platform-express";
+import { MulterModule } from "@nestjs/platform-express";
 import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-  @Get("users")
-  getUsers(@Query("a") a: string): Services.User[] {
-    console.log("%c ~ name ccc ~ ", "color:#2ecc71", a);
-    return this.appService.getUsers(a);
-  }
   @Post("users")
-  editUser(@Body() a) {
-    // editUser(@Req() a) {
-    console.log("%c ~ a ?? ~ ", "color:#2ecc71", a);
-    return this.appService.editUser(a);
+  @UseInterceptors(FilesInterceptor("file", 3))
+  async uploadFormData(
+    @UploadedFiles() files,
+    @Body() body: { name: string; b: string }
+  ) {
+    console.log("Files:", files);
+    console.log("Body:", body);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "public"),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
